6e14ed62825a5edbc71d1157ff37d2b9e998210c
pcanabarro <pedroh.canabarro@gmail.com>
2024-07-22 14:19:08 -0300

    Changing language to Java

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM node:21
-WORKDIR /usr/src/app
-COPY package*.json ./
-RUN npm install
-COPY . .
-EXPOSE 3001
-USER node
-CMD ["npm", "start"]
---
+++ src/test/java/com/pcanabarro/url_shortener/UrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pcanabarro.url_shortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
