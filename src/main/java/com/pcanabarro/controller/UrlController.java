1dd6af9ed025273eea87e88a8f0142308f976605
pcanabarro <pedroh.canabarro@gmail.com>
2024-08-16 20:29:30 -0300

    logging delete url

--- src/main/java/com/pcanabarro/controller/UrlController.java
+++ src/main/java/com/pcanabarro/controller/UrlController.java
@@ -108,6 +108,7 @@ public class UrlController {
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteUrl(@PathVariable long id) {
         urlService.deleteUrl(id);
+        log.error("Url {} was deleted successfully ", id);
 
         return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Url with id " + id + " was deleted");
     }
